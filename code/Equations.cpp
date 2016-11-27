f5f5016b8258c641d5e751b5922e3a41d87b7e3d
alorkowski <alexander.lorkowski@epfl.ch>
2016-11-27 23:11:34 +0100

    Minor changes to the Equations class
    	modified:   Equations.cpp

--- code/Equations.cpp
+++ code/Equations.cpp
@@ -11,7 +11,7 @@ Equations::~Equations() {}
 /// Inputs N - Power of Polynomial needs to be given in order. coef of x1^1..x2^2 --> a_0 + a_1 ... etc
 
 double Equations::getPolyEquation(const std::vector<double> &coef, double value) {
-	double f;
+	double f = 0.0;
     for (unsigned int i = 0; i < coef.size(); ++i) {
         	f += coef[i]*pow(value, i);
     }
@@ -21,7 +21,10 @@ double Equations::getPolyEquation(const std::vector<double> &coef, double value)
 double Equations::getPolyDerivative(const std::vector<double> &coef, double value) {
     // Set an exception for methods where the derivative is a denominator.
 	// This way we can avoid null errors (this extends to 0.0 as well).
-    double df;
+	// Different compilers/settings may treat uninitialized variables differently, so we should
+	// initialize this (i.e. crash/error when the original function is a constant).
+	// g++ w/ c++11 compiled program automatically defaults to 0.0 anyway.
+    double df = 0.0;
     for (unsigned int i = 1; i < coef.size(); ++i) {
     		df += i*coef[i]*pow(value, i-1);
     }
