5b882c77258c0a9cfbb96a5bd40c5153c57c7456
Michael Jaquier <shibasu@gmail.com>
2016-11-29 18:10:40 +0100

    Minor refactor

--- code/TestSuit.cpp
+++ code/TestSuit.cpp
@@ -8,23 +8,37 @@
 
 int main(int argc, char* argv[]) {
     TestSuit test;
-    test.testChordSolver(0.0015, 0.66381, 3, 1000, false);
-    test.testNewtonSolver(0.0015, -0.66381, 3, 1000, false);
-    test.testNewtonWithExprtkPoly(0.0015, -3.16227766517654, 1.1, 1000, false);
-    test.testNewtonWithExprtkLog(0.0015, 22026.4657948162, 10.0, 1000, false);
-    test.testNewtonWithExprtTrig(0.0015, -1.62499999969059, 1, 1000, false);
-    test.testNewtonWithExprtExp(0.0015, -0.095561, 0.1, 1000, false);
+
+    // Equations
+    std::string mPolyCoefficient = "x^2 -10";
+    std::string mLogCoefficient = "log(x) - 10";
+    std::string mTrigCoefficient = "cos(2*pi*x) + sin(2*pi*x)";
+    std::string mExpCoefficient = "exp(x) - exp(-3x) + exp(9x)";
+
+    // Vectors
+    const std::vector<double> mTestVector{3.0, -4.0, 10.0, -22.0, 10.0, -2.0};
+
+    // Tests
+    test.testChordSolver(0.0015, 0.66381, 3, 1000, false, mTestVector);
+    test.testNewtonSolver(0.0015, -0.66381, 3, 1000, false, mTestVector);
+    test.testNewtonWithExprtkPoly(0.0015, -3.16227766517654, 1.1, 1000, false,
+                                  mPolyCoefficient);
+    test.testNewtonWithExprtkLog(0.0015, 22026.4657948162, 10.0, 1000, false,
+                                 mLogCoefficient);
+    test.testNewtonWithExprtTrig(0.0015, -1.62499999969059, 1, 1000, false,
+                                 mTrigCoefficient);
+    test.testNewtonWithExprtExp(0.0015, -0.095561, 0.1, 1000, false,
+                                mExpCoefficient);
 };
 
 void
 TestSuit::testChordSolver(const double tol, const double expected,
-                          const int x0, const int max,
-                          const bool verbose) {
-    std::vector<double> testVector{3.0, -4.0, 10.0, -22.0, 10.0, -2.0};
+                          const int x0, const int max, const bool verbose,
+                          const std::vector<double> &vector) {
     Chord testChord;
     double *realValue = new double;
     *realValue = testChord.chordSolver(
-            (const std::vector<double> &) testVector, x0, tol, max, verbose);
+            vector, x0, tol, max, verbose);
     assert((expected - *realValue) <= tol);
     printf("Chord Success");
     printf("\n");
@@ -34,12 +48,13 @@ TestSuit::testChordSolver(const double tol, const double expected,
 void
 TestSuit::testNewtonSolver(const double tol, const double expected,
                            const double x0, const int max,
-                           const bool verbose) {
-    std::vector<double> testVector{3.0, -4.0, 10.0, -22.0, 10.0, -2.0};
+                           const bool verbose,
+                           const std::vector<double> &vector) {
+    //std::vector<double> testVector{3.0, -4.0, 10.0, -22.0, 10.0, -2.0};
     Newton testNewton;
     double *realValue = new double;
     *realValue = testNewton.newtonSolver(
-            (const std::vector<double> &) testVector, x0, tol, max, verbose);
+            vector, x0, tol, max, verbose);
     assert((expected - *realValue) <= tol);
     printf("Newton: Poly Success");
     printf("\n");
@@ -48,13 +63,12 @@ TestSuit::testNewtonSolver(const double tol, const double expected,
 void
 TestSuit::testNewtonWithExprtkPoly(const double tol, const double expected,
                                    const double x0, const int max,
-                                   const bool verbose) {
+                                   const bool verbose,
+                                   std::string &coefficient) {
     double testNewton;
-    std::string coefficient;
-    coefficient = "x^2 -10";
     //TODO: Refactor this into a constructor along with other parameters we can
     Newton mNewton;
-    testNewton = mNewton.newtonExprtkSolver((std::string &) coefficient, x0,
+    testNewton = mNewton.newtonExprtkSolver(coefficient, x0,
                                             tol, max, verbose);
     assert((expected - testNewton) <= tol);
     printf("Newton: Exprtk Poly Success");
@@ -64,12 +78,11 @@ TestSuit::testNewtonWithExprtkPoly(const double tol, const double expected,
 void
 TestSuit::testNewtonWithExprtkLog(const double tol, const double expected,
                                   const double x0, const int max,
-                                  const bool verbose) {
+                                  const bool verbose,
+                                  std::string &coefficient) {
     double testNewton;
-    std::string coefficient;
-    coefficient = "log(x) - 10";
     Newton mNewton;
-    testNewton = mNewton.newtonExprtkSolver((std::string &) coefficient, x0,
+    testNewton = mNewton.newtonExprtkSolver(coefficient, x0,
                                             tol, max, verbose);
 
     assert((expected - testNewton) <= tol);
@@ -80,9 +93,9 @@ TestSuit::testNewtonWithExprtkLog(const double tol, const double expected,
 void
 TestSuit::testNewtonWithExprtTrig(const double tol, const double expected,
                                   const double x0, const int max,
-                                  const bool verbose) {
+                                  const bool verbose,
+                                  std::string &coefficient) {
     double testNewton;
-    std::string coefficient;
     coefficient = "cos(2*pi*x) + sin(2*pi*x)";
     Newton mNewton;
     //TODO: Testing periodicity so we aren't so hamstrung by starting point
@@ -96,10 +109,9 @@ TestSuit::testNewtonWithExprtTrig(const double tol, const double expected,
 void
 TestSuit::testNewtonWithExprtExp(const double tol, const double expected,
                                  const double x0, const int max,
-                                 const bool verbose) {
+                                 const bool verbose,
+                                 std::string &coefficient) {
     double testNewton;
-    std::string coefficient;
-    coefficient = "exp(x) - exp(-3x) + exp(9x)";
     Newton mNewton;
     testNewton = mNewton.newtonExprtkSolver((std::string &) coefficient, x0,
                                             tol, max, verbose);
