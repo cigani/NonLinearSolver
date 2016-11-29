de904f6e1baf6ea44685560d10ca0672ffbd9263
alorkowski <alexander.lorkowski@epfl.ch>
2016-11-29 19:26:32 +0100

    Program now accepts command line arguments.  Run program with no arguments for a help dialog.
    	modified:   Main.cpp

--- code/Main.cpp
+++ code/Main.cpp
@@ -18,14 +18,86 @@
 #include "Equations.hpp"
 #include "Chord.hpp"
 #include "Newton.hpp"
+#include "Aitken.hpp"
+
+std::string stringPadding(std::string original, size_t charCount);
+static void show_usage(std::string name);
+static void show_methods();
 
 int main(int argc, char* argv[]) {
 
-	std::string coefficient;
-	std::vector<double> coefVector;
-	double x0;
-	Equations mEquation;
+	if (argc < 3) {
+	        show_usage(argv[0]);
+	        return 1;
+	}
 
+	std::string mMethod;
+	std::string mExpression;
+	double x0;
+	int nMax = 1000;
+	double tol = 0.001;
+	bool verbose = true;
+
+	// Parse the command line arguments for flags and values
+	for (int i = 1; i < argc; i++) {
+		if (i + 1 != argc) {
+			if (( strcmp( argv[i], "-m") == 0) || ( strcmp( argv[i], "--method") == 0)) {
+				// Method declaration
+				if ( strcmp( argv[i + 1], "aitken") == 0 ) {
+					mMethod = argv[i + 1];
+				} else if ( strcmp( argv[i + 1], "chord") == 0 ) {
+					mMethod = argv[i + 1];
+				} else if ( strcmp( argv[i + 1], "fixedpoint") == 0 ) {
+					mMethod = argv[i + 1];
+				} else if ( strcmp( argv[i + 1], "newton") == 0 ) {
+					mMethod = argv[i + 1];
+				} else {
+					std::cout << std::endl << "ERROR: Unknown method provided" << std::endl;
+					show_methods();
+					return 1;
+				}
+			} else if ( strcmp( argv[i], "-e") == 0) {
+				// Mathematical Expression
+				mExpression = argv[i + 1];
+			} else if ( strcmp( argv[i], "-xi") == 0) {
+				// Initial Value
+				x0 = std::stod(argv[i + 1]);
+			} else if ( strcmp( argv[i], "-nmax") == 0) {
+				// Maximum number of iterations.  Default 1000 as defined above.
+				nMax = std::stoi(argv[i + 1]);
+			} else if ( strcmp( argv[i], "-t") == 0) {
+				// Maximum number of iterations.  Default 1000 as defined above.
+				tol = std::stod(argv[i + 1]);
+			} else if ( strcmp( argv[i], "-v") == 0) {
+				// Maximum number of iterations.  Default 1000 as defined above.
+				verbose = argv[i + 1];
+			} else if (( strcmp( argv[i], "-h") == 0) || ( strcmp( argv[i], "--help") == 0)) {
+				// Print help dialog
+				show_usage(argv[0]);
+			} else {
+				continue;
+			}
+		}
+	}
+
+	if ( mMethod == "fixedpoint") {
+		std::cout << "Calling the Fixed Point Method" << std::endl;
+		FixedPoint fixedPoint;
+	} else if ( mMethod == "newton") {
+		std::cout << "Calling the Newton Method" << std::endl;
+		Newton newton;
+		double result = newton.newtonExprtkSolver(mExpression, x0, tol, nMax, verbose);
+		std::cout << result << std::endl;
+	} else if ( mMethod == "chord") {
+		std::cout << "Calling the Chord Method" << std::endl;
+		Chord chord;
+	} else if ( mMethod == "aitken") {
+		std::cout << "Calling the Aitken Method" << std::endl;
+		Aitken aitken;
+	}
+}
+
+	/*
 	int i = 0;
 	while (true){
 		std::cout << "Enter coefficient for " << "x^" << i << " " << "Terminate with a blank line: ";
@@ -37,33 +109,20 @@ int main(int argc, char* argv[]) {
 	    	coefVector.push_back(stod(coefficient));
 	    }
 	    ++i;
-	    /*
+
         std::cout << "Enter Equation ";
 		getline(std::cin, coefficient);
-        std::cout << "Enter Starting Value";
         break;
-        */
 	}
 
-	// The following loop is for only development purpose
-	std::cout << "Enter initial value: ";
-	std::cin >> x0;
-	std::cout << std::endl;
+	double f = mEquation.getPolyEquation((const std::vector<double> &) coefVector, x0);
+	double df = mEquation.getPolyDerivative((const std::vector<double> &) coefVector, x0);
+    double ff = mEquation.exprtkGenerate2D((std::string &) coefficient, x0);
+    double dff = mEquation.exprtkGenerate2DDerivative(
+            (std::string &) coefficient, x0);
 
-	/*
-	for (std::vector<double>::const_iterator i = coefVector.begin(); i != coefVector.end(); ++i) {
-	    std::cout << *i << ' ';
-	}
-	*/
-
-//	double f = mEquation.getPolyEquation((const std::vector<double> &) coefVector, x0);
-//	double df = mEquation.getPolyDerivative((const std::vector<double> &) coefVector, x0);
-//    double ff = mEquation.exprtkGenerate2D((std::string &) coefficient, x0);
-//    double dff = mEquation.exprtkGenerate2DDerivative(
-//            (std::string &) coefficient, x0);
-//
-//    printf("Result Equation: %10.5f\n", ff);
-//    printf("Result Derivative: %10.5f\n", dff);
+    printf("Result Equation: %10.5f\n", ff);
+    printf("Result Derivative: %10.5f\n", dff);
 
 	// Currently uses cosine as the iteration function
 	FixedPoint mFixedPoint;
@@ -76,4 +135,77 @@ int main(int argc, char* argv[]) {
 	
     std::cout << result << std::endl;
     return 0;
+}
+*/
+
+std::string stringPadding(std::string original, size_t charCount ) {
+    original.resize( charCount, ' ' );
+    return original;
+}
+
+static void show_usage(std::string name) {
+	    std::cerr << std::endl
+				<< "Usage: "
+				<< name
+				<< " -m <>"
+				<< " -e <>"
+				<< " -xi <>"
+				<< " -nmax <>"
+				<< " -t <>"
+				<< " -v <>"
+				<< std::endl
+				<< std::endl
+	    		<< "Options:\n"
+				<< "\t"
+				<< stringPadding("-h,--help", 20)
+				<< stringPadding("Show this help message", 40)
+				<< std::endl
+				<< "\t"
+				<< stringPadding("-m,--method", 20)
+				<< stringPadding("Specify Non-Linear Solver", 40)
+				<< std::endl
+				<< "\t"
+				<< stringPadding("-e,--expression", 20)
+				<< stringPadding("Input a mathematical expression to solve", 40)
+				<< std::endl
+				<< "\t"
+				<< stringPadding("-xi", 20)
+				<< stringPadding("Initial guess of the solution", 40)
+				<< std::endl
+				<< "\t"
+				<< stringPadding("-nmax", 20)
+				<< stringPadding("Maximum number of iterations [default: 1000]", 40)
+				<< std::endl
+				<< "\t"
+				<< stringPadding("-t", 20)
+				<< stringPadding("The convergence tolerance [default: 0.001]", 40)
+				<< std::endl
+				<< "\t"
+				<< stringPadding("-v", 20)
+				<< stringPadding("True to print all intermediate calculations [default: false]", 40)
+				<< std::endl;
+	    show_methods();
+}
+
+static void show_methods() {
+	    std::cerr << std::endl
+	    		<< "Non-Linear Solvers:"
+	    		<< std::endl
+				<< "\t"
+				<< stringPadding("aitken", 20)
+				<< stringPadding("Aitken Method", 40)
+				<< std::endl
+	    		<< "\t"
+	    		<< stringPadding("chord", 20)
+				<< stringPadding("Chord Method", 40)
+				<< std::endl
+				<< "\t"
+				<< stringPadding("fixedpoint", 20)
+				<< stringPadding("Fixed Point Method", 40)
+				<< std::endl
+				<< "\t"
+				<< stringPadding("newton", 20)
+				<< stringPadding("Newton Method", 40)
+				<< std::endl
+				<< std::endl;
 }
